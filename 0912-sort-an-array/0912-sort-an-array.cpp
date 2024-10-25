cddde980c610f066826f9e09c00730e20dc10d52
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2024-10-25 11:13:20 +0530

    Time: 113 ms (73.63%), Space: 98.4 MB (46.27%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,9 +1,23 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        unordered_map<int, int>mp;
+        for(int &num: nums){
+            mp[num]++;
+        }
         
+        int minE = *min_element(nums.begin(),nums.end());
+        int maxE = *max_element(nums.begin(), nums.end());
+        
+        int i = 0;
+        
+        for(int num = minE; num<= maxE; num++){
+            while(mp[num] >0 ){
+                nums[i] = num;
+                i++;
+                mp[num]--;
+            }
+        }
         return nums;
-        
     }
 };
