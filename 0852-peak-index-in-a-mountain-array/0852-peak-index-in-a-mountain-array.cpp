1fc3809372fcd9db996a928fda221488f21b7f6c
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2024-10-27 13:33:51 +0530

    Time: 0 ms (100.00%), Space: 62.5 MB (47.91%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    int peakIndexInMountainArray(vector<int>& arr) {
-        int ans = 0;
-        int max = *max_element(arr.begin(), arr.end());
-        for(int i = 0; i< arr.size(); i++){
-            if(arr[i] == max){
-                ans = i;
-            }
-        }
-        return ans;
+     int peakIndexInMountainArray(vector<int>& arr) {
+        int n = arr.size();
+        
+        int i = 0;
+       //You can remove i < n-1 check because array is gauranteed to be Mountain (given in qn)
+        while(i < n-1 && arr[i] < arr[i+1])
+            i++;
+        
+        return i;
     }
 };
