26bd1563e71c034344fce2a164e2f8b58215923d
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2024-10-29 00:39:14 +0530

    Time: 0 ms (100.00%), Space: 9.2 MB (90.50%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -4,7 +4,7 @@ public:
          vector<int>ans(n + 1);
 
     for (int i = 0; i <= n; ++i) {
-        ans[i] = ans[i >> 1] + (i & 1);
+        ans[i] = __builtin_popcount(i);
     }
     return ans;
         
