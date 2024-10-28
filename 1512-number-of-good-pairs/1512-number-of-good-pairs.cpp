3a2a665079e148e1176f3ce0c62e1d45b347e658
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2024-10-28 15:54:30 +0530

    Time: 0 ms (100.00%), Space: 9 MB (78.22%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-    static bool myComparator(pair<int, int>& a, pair<int, int>& b) {
-        return a.second > b.second;
-    }
-
     int numIdenticalPairs(vector<int>& nums) {
-        unordered_map<int, int> freq;
-        for (int i = 0; i < nums.size(); i++) {
-            freq[nums[i]]++;
-        }
-
-        vector<pair<int, int>> freqVec(freq.begin(), freq.end());
-
-        sort(freqVec.begin(), freqVec.end(), myComparator);
+        unordered_map<int, int> mp;
         
-        int goodPairs = 0;
-        for (int i = 0; i < freqVec.size(); i++) {
-            int n = freqVec[i].second;
-            if (n > 1) {
-                goodPairs += (n * (n - 1)) / 2;
-            }
+        for(int &num : nums) {
+            mp[num]++;
         }
-
-        return goodPairs;
+        
+        int result = 0;
+        for(auto &it : mp) {
+            
+            int count = it.second;
+            result += (count * (count-1))/2;
+            
+        }
+        
+        return result;
+        
     }
 };
