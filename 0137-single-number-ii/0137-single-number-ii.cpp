68571a2a4e6b8c56cc3797439d0f2b3e8fda2684
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2024-10-28 23:56:48 +0530

    Time: 0 ms (100.00%), Space: 13.8 MB (14.83%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int> freq;
+        
+        for(int num : nums){
+            freq[num]++;
+        }
+
+
+        for (const auto& pair : freq) {
+            if (pair.second == 1) {
+                return pair.first;
+            }
+        }
+
+        return -1;
+    }
+};
