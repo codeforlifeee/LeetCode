fbed020b7a84dbe73bde1c96e9e35760ce0d721d
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2024-10-28 15:30:02 +0530

    Time: 83 ms (39.82%), Space: 26.5 MB (86.77%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -3,18 +3,18 @@ public:
     int equalPairs(vector<vector<int>>& grid) {
         int n = grid.size();
         int count = 0;
-        //transpose
-        vector<vector<int>> transposed(n, vector<int>(n));
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < n; ++j) {
-                transposed[j][i] = grid[i][j];
-            }
-        }
-        //comparing
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (grid[i] == transposed[j]) {
-                    ++count;  // Found a matching pair
+                bool isEqual = true;
+                for (int k = 0; k < n; ++k) {
+                    if (grid[i][k] != grid[k][j]) {
+                        isEqual = false;
+                        break;
+                    }
+                }
+
+                if (isEqual) {
+                    ++count;
                 }
             }
         }
