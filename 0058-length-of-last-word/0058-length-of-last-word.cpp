d7b5003e318403c63e8146c9fa485f5f8c393bf5
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2024-10-28 12:12:09 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (5.84%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int res = 0, temp = 0;
+        stringstream ss(s);
+        string word;
+        string lastWord;
 
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == ' ' && s[i + 1] != ' ') {
-                res = 0;
-            } else
-                res++;
-            if (s[i] != ' ' && (s[i + 1] == ' ' || i + 1 == s.length())) {
-                temp = res;
-            }
-        }
-        return temp;
+
+    while (ss >> word) {
+        lastWord = word; 
+    }
+    return lastWord.length();
+        
+        
+
+        
     }
 };
