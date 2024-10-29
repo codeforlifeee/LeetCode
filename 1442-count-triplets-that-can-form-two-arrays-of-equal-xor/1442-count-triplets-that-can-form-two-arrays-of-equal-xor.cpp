dde504e56ddebca2482369ac8a7f886f1077b237
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2024-10-29 13:32:50 +0530

    Time: 1 ms (31.20%), Space: 9.6 MB (28.05%) - LeetHub

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     int countTriplets(vector<int>& arr) {
-        int n = arr.size();
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int a = 0;
-                for (int x = i; x < j; x++) {
-                    a ^= arr[x];
+        vector<int> prefixXor(begin(arr), end(arr));
+        prefixXor.insert(prefixXor.begin(), 0);
+        int n = prefixXor.size();
+
+        for(int i = 1; i < n; i++) {
+            prefixXor[i] ^= prefixXor[i-1];
+        }
+        int triplets = 0;
+
+        for(int i = 0; i < n; i++) {
+            for(int k = i+1; k < n; k++) {
+
+                if(prefixXor[k] == prefixXor[i]) {
+                    triplets += k-i-1;
                 }
 
-                int b = 0;
-                for (int k = j; k < n; k++) {
-                    b ^= arr[k];
-                    if (a == b) {
-                        count++;
-                    }
-                }
             }
         }
-        return count;
+        return triplets;
     }
 };
