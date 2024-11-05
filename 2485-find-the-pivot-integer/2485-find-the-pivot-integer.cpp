4e598f339a248a0ff41a539ac38b40dc3a52c9f2
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2024-11-05 16:51:10 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (85.74%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -4,18 +4,11 @@ public:
         if(n == 1){
             return 1;
         }
-        
         int totalSum = n*(n+1)/2;
+        int pivot = sqrt(totalSum);
         
-        for(int pivot = 1; pivot<=n;pivot++) {
-            
-            int leftSum  = pivot*(pivot+1)/2;
-            int rightSum = totalSum - leftSum + pivot;
-            
-            if(leftSum == rightSum){
-                return pivot;
-            }
-        }
+        if(pivot*pivot == totalSum)
+            return pivot;
         
         return -1;
     }
