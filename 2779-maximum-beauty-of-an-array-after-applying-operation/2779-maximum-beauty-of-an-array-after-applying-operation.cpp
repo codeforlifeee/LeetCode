7f512628c540c7323fba1fc0e271e97639a4d0f9
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2024-12-14 12:31:52 +0530

    Time: 83 ms (22.42%), Space: 105 MB (82.35%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
-    int maximumBeauty(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        int maxBeauty = 0;
-
-        for (int i = 0; i < nums.size(); i++) {
-            // Find the farthest index where the value is within the range
-            // [nums[i], nums[i] + 2*k]
-            int upperBound = findUpperBound(nums, nums[i] + 2 * k);
-            // Update the maximum beauty based on the current range
-            maxBeauty = max(maxBeauty, upperBound - i + 1);
-        }
-        return maxBeauty;
-    }
-
-private:
-    // Helper function to find the largest index where arr[index] <= val
-    int findUpperBound(vector<int>& arr, int val) {
-        int low = 0, high = arr.size() - 1, result = 0;
-
-        // Perform binary search to find the upper bound
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (arr[mid] <= val) {
-                result = mid;  // Update the result and move to the right half
-                low = mid + 1;
+    int binarySearch(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        int result = -1;
+        
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            
+            if (nums[mid] <= target) {
+                result = mid;  
+                l = mid + 1;  
             } else {
-                high = mid - 1;  // Move to the left half
+                r = mid - 1;  
             }
         }
+        
         return result;
+    }
+    
+    int maximumBeauty(vector<int>& nums, int k) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        
+        int maxBeauty = 0;
+        
+        for (int i = 0; i < n; i++) {
+            int x = nums[i];
+            int y = x + 2 * k;
+            int j = binarySearch(nums, y); 
+            
+            maxBeauty = max(maxBeauty, j - i + 1);
+        }
+        
+        return maxBeauty;
     }
 };
