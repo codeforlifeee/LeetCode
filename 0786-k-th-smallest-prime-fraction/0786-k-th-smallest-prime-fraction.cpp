9ab50cae4445682024a893a61250ff5b53fc33bf
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2024-12-16 17:33:03 +0530

    Time: 415 ms (36.31%), Space: 120.6 MB (34.11%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
@@ -3,19 +3,18 @@ public:
      typedef pair<int, int> P;
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
         int n = arr.size();
-        priority_queue<pair<double, P>, vector<pair<double, P>>, greater<pair<double, P>>> minHeap;
-        
+        priority_queue<pair<double, P>> maxHeap;        
         for(int i = 0;i < n;i++){
             for(int j = i+1;j < n;j++){
                 double value = (double)arr[i]/arr[j];  
-                minHeap.push({value, {arr[i], arr[j]}});
+                maxHeap.push({value, {arr[i], arr[j]}});
             }
         }
 
-        for (int i = 0;i < k-1;i++){
-            minHeap.pop();  
+        while(maxHeap.size() > k){
+            maxHeap.pop();  
         }
-        pair<double, P> result = minHeap.top();
+        pair<double, P> result = maxHeap.top();
         return {result.second.first, result.second.second};
     }
 };
