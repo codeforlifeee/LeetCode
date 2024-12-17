fa7fdc5652edfdd26fba638b726e3c302cc11938
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2024-12-17 18:48:23 +0530

    Time: 59 ms (39.01%), Space: 27.3 MB (42.67%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -1,34 +1,19 @@
 class Solution {
+    //LIS Code is used Here//with sorting//
 public:
-    int n;
-    int t[1001][1001];
-    int lis(vector<vector<int>>& nums, int prev_idx, int curr_idx){
-       if(curr_idx == n)
-           return 0;
-        
-        if(prev_idx != -1 && t[prev_idx][curr_idx] != -1)
-            return t[prev_idx][curr_idx];
-        
-        int taken = 0;
-        if(prev_idx == -1 || nums[curr_idx][0] > nums[prev_idx][1])
-            taken = 1 + lis(nums, curr_idx, curr_idx+1);
-        
-        int not_taken = lis(nums, prev_idx, curr_idx+1);
-        
-        if(prev_idx != -1)
-            t[prev_idx][curr_idx] =  max(taken, not_taken);
-        
-        return max(taken, not_taken);
-            
-    }
-    int findLongestChain(vector<vector<int>>& pairs) {
-        memset(t, -1, sizeof(t));
-        n = pairs.size();
-        //lis code is used here//
-        sort(pairs.begin(), pairs.end());
-        return lis(pairs, -1, 0);
-        
-        
+    int findLongestChain(vector<vector<int>>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<int> t(n, 1);
+        int maxL = 1;
+        for(int i = 0; i<n; i++) {
+            for(int j = 0; j<i; j++) {
+                if(nums[j][1] < nums[i][0]) {
+                    t[i] = max(t[i], t[j]+1);
+                    maxL = max(maxL, t[i]);
+                }
+            }
+        }
+        return maxL;
     }
 };
- 
