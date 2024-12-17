be3e9a7c7d3ecedfd3b02714b84f8f90bf7e3ac9
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2024-12-18 01:11:43 +0530

    Time: 23 ms (80.09%), Space: 27.4 MB (23.05%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string s1, string s2) {
+        int m = s1.length();
+        int n = s2.length();
+        
+        vector<vector<int>> t(m+1, vector<int>(n+1));
+        //first row and first column would have zero value
+        for(int row = 0; row < m+1; row++){
+            t[row][0] = 0;   
+        }
+        for(int col = 0; col < n+1; col++){
+            t[0][col] = 0;   
+        }
+        
+        for(int i = 1; i< m+1; i++){
+            for(int j = 1; j<n+1; j++){
+                
+                if(s1[i-1] == s2[j-1]){ 
+                    t[i][j] = 1 + t[i-1][j-1];
+                } else {
+                    t[i][j] = max(t[i-1][j],t[i][j-1]);
+                }
+            }
+        }
+        
+        string lcs = "";
+        int i = m, j = n;
+        
+        while(i>0 && j>0){
+            if(s1[i-1] == s2[j-1]){
+                lcs.push_back(s1[i-1]);
+                i--;
+                j--;
+            }else{
+                if(t[i-1][j] > t[i][j-1]){
+                    i--;
+                }else{
+                    j--;
+                }
+            }
+        }
+        reverse(lcs.begin(), lcs.end());
+        
+        //cout<<lcs<<endl;
+        
+        return t[m][n];
+    }
+};
