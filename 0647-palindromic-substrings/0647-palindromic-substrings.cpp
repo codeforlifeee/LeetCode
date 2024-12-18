9783a8e0818b4793bab09652ced86fc270404020
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2024-12-18 16:58:24 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (72.22%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
+    int count;
+    
+    void check(string& s, int i, int j, int n){
+        while( i>= 0 && j<n && s[i] == s[j]){
+            count++;
+            i--;
+            j++;
+        }
+    }
+        
     int countSubstrings(string s) {
         int n = s.length();
         
-        vector<vector<bool>>t(n, vector<bool>(n,false));
-        
-        int count = 0;
-        
-        for(int L =1; L<=n;L++){
-            for(int i = 0; i+L-1 < n; i++){
-                int j = i+L-1;
-                
-                if(i==j){ t[i][i] = true; }
-                else if( i+1 == j ){ t[i][j] = (s[i] == s[j]); }
-                else { t[i][j] = (s[i] == s[j] && t[i+1][j-1]); }
-                
-                if(t[i][j] == true){ count++; }
-            }
+        for(int i = 0; i<n; i++){
+            check(s,i,i,n);
+            check(s,i,i+1,n);
         }
         return count;
     }
