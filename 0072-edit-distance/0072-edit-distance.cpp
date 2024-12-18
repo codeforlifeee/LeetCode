ec48628eb4cddd7ffb8345f9591a1067df8b1e4b
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2024-12-18 14:29:20 +0530

    Time: 11 ms (28.44%), Space: 13.1 MB (35.74%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    int m,n;
-    int t[501][501];
-    
-    int solve(string& s1, string& s2, int m, int n){
-        
-        if(m==0){ return n; }//insert in s1
-        else if(n==0){ return m; }//delete from s1
-        
-        if(t[m][n] != -1){ return t[m][n]; }
-        
-        if(s1[m-1] == s2[n-1]){ return t[m][n] = solve(s1,s2,m-1,n-1);}
-        else{
-            int insertC  = 1 + solve(s1,s2,m,n-1);
-            int deleteC  = 1 + solve(s1,s2,m-1,n);
-            int replaceC = 1 + solve(s1,s2,m-1,n-1);
-            
-            return t[m][n] = min({insertC, deleteC, replaceC});
-        }
-        return -1;
-    }
     int minDistance(string s1, string s2) {
+        int m = s1.length();
+        int n = s2.length();
         
-        memset(t, -1, sizeof(t));
-        
-        m = s1.length();
-        n = s2.length();
-        
-        return solve(s1, s2, m, n);
+        vector<vector<int>> t(m+1, vector<int>(n+1));
+
+        // Fill DP table
+        for(int i = 0; i <= m; i++) {
+            for(int j = 0; j <= n; j++) {
+                // If one of the strings is empty
+                if(i == 0 || j == 0) {
+                    t[i][j] = i + j;
+                }
+                // If characters match, no operation needed
+                else if(s1[i-1] == s2[j-1]) {
+                    t[i][j] = t[i-1][j-1];
+                }
+                // If characters don't match, take the minimum of the three possible operations
+                else {
+                    t[i][j] = 1 + min({t[i][j-1], t[i-1][j], t[i-1][j-1]});
+                }
+            }
+        }
+        // The answer is in the bottom-right cell
+        return t[m][n];
     }
 };
