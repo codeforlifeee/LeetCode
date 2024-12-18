01783c14cdd2003f367df1655a218744609f3ef4
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2024-12-18 16:40:53 +0530

    Time: 31 ms (30.53%), Space: 11.2 MB (57.39%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,37 +1,23 @@
 class Solution {
 public:
-    int t[1001][1001];
-    
-    bool check(string &s, int i, int j){
-        if(i > j) return true;
-        
-        if(t[i][j] != -1) return t[i][j]; //0 : false 1: true
-        
-        if(s[i] == s[j]) return t[i][j] = check(s, i+1, j-1);
-        
-        return t[i][j] = false;
-    }
-    
     int countSubstrings(string s) {
-        
-        memset(t,-1,sizeof(t));
-        //t[i][j] = -1: not solved yet
-        //t[i][j] = 0 : False
-        //t[i][j] = 1 : True
-        
-        
         int n = s.length();
+        
+        vector<vector<bool>>t(n, vector<bool>(n,false));
+        
         int count = 0;
         
-        for(int i = 0; i<n; i++){
-            for(int j = i; j<n; j++){
+        for(int L =1; L<=n;L++){
+            for(int i = 0; i+L-1 < n; i++){
+                int j = i+L-1;
                 
-                if(check(s,i,j)){
-                    count++;
-                }
+                if(i==j){ t[i][i] = true; }
+                else if( i+1 == j ){ t[i][j] = (s[i] == s[j]); }
+                else { t[i][j] = (s[i] == s[j] && t[i+1][j-1]); }
+                
+                if(t[i][j] == true){ count++; }
             }
         }
         return count;
-        
     }
 };
