353bce1b8528551f275872ab652be9a3512cac8b
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2024-12-18 14:18:30 +0530

    Time: 47 ms (17.17%), Space: 11.6 MB (84.65%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -3,20 +3,20 @@ public:
     int m,n;
     int t[501][501];
     
-    int solve(string& s1, string& s2, int i, int j){
+    int solve(string& s1, string& s2, int m, int n){
         
-        if(i==m){ return n-j; }//insert in s1
-        else if(j ==n){ return m-i; }//delete from s1
+        if(m==0){ return n; }//insert in s1
+        else if(n==0){ return m; }//delete from s1
         
-        if(t[i][j] != -1){ return t[i][j]; }
+        if(t[m][n] != -1){ return t[m][n]; }
         
-        if(s1[i] == s2[j]){ return t[i][j] = solve(s1,s2,i+1,j+1);}
+        if(s1[m-1] == s2[n-1]){ return t[m][n] = solve(s1,s2,m-1,n-1);}
         else{
-            int insertC  = 1 + solve(s1,s2,i,j+1);
-            int deleteC  = 1 + solve(s1,s2,i+1,j);
-            int replaceC = 1 + solve(s1,s2,i+1,j+1);
+            int insertC  = 1 + solve(s1,s2,m,n-1);
+            int deleteC  = 1 + solve(s1,s2,m-1,n);
+            int replaceC = 1 + solve(s1,s2,m-1,n-1);
             
-            return t[i][j] = min({insertC, deleteC, replaceC});
+            return t[m][n] = min({insertC, deleteC, replaceC});
         }
         return -1;
     }
@@ -27,6 +27,6 @@ public:
         m = s1.length();
         n = s2.length();
         
-        return solve(s1, s2, 0, 0);
+        return solve(s1, s2, m, n);
     }
 };
