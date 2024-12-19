fda53da5aaa3423f616ba2bad0664f2c761aa6fc
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2024-12-19 17:43:50 +0530

    Time: 0 ms (100.00%), Space: 9.3 MB (21.19%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
@@ -3,11 +3,9 @@ public:
     string largestGoodInteger(string num) {
        vector<string>req = {"999", "888","777","666","555","444","333","222","111","000"};
         
-        for(const string& s : req){
-            for(int i = 0;i<=num.length()-3;i++){
-                if(num.substr(i, 3) == s){
-                    return s;
-                }
+       for(const string& s : req){
+            if(num.find(s) != string::npos){
+                return s;
             }
         }
         return "";
