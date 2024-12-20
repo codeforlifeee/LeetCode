fb82bbe6a42724723efc82f8f7cfde0433fa1845
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2024-12-20 14:36:19 +0530

    Time: 2 ms (35.82%), Space: 17.1 MB (20.88%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
@@ -5,30 +5,28 @@ public:
         int n = mat[0].size();
         int result = 0;
         
+        vector<int>rowCount(m,0);
+        vector<int>colCount(n,0);
+        
         for(int i =0; i<m; i++){
             for(int j = 0; j<n;j++){
                 
-                if(mat[i][j] == 0) continue;
-                
-                bool special = true;
-                
-                //check column
-                for(int r =0; r<m; r++){
-                    if(r != i && mat[r][j] == 1){
-                        special = false;
-                        break;
-                    }
+                if(mat[i][j] == 1) {
+                    rowCount[i]++;
+                    colCount[j]++;
                 }
-                //check row
-                for(int c = 0; c<n; c++){
-                    if(c != j && mat[i][c] == 1){
-                        special = false;
-                        break;
-                    }
-                }
-                if(special == true) result++;
+    
             }
         }
-        return result; 
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                if(mat[i][j] == 0) continue;
+                
+                if(rowCount[i] == 1 && colCount[j] ==  1){
+                    result++;
+                }
+            }
+        }
+        return result;
     }
 };
