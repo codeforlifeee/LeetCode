17e288c1525821aad2625af8def7efa0b3b77c99
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2024-12-21 22:35:07 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (26.74%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -4,24 +4,22 @@ public:
         int n = s.length();
         
         int start_with_0 = 0;
-        int start_with_1 = 0;
         
         for(int i = 0; i<n; i++){
             if(i%2 == 0){
-                if(s[i] == '0'){
-                    start_with_1++;
-                }else{
+                if(s[i] == '1'){
                     start_with_0++;
                 }
             }else{
-                if(s[i] == '1'){
-                    start_with_1++;
-                }else{
+                if(s[i] == '0'){
                     start_with_0++;
                 }
             }
         }
         
-        return min(start_with_0, start_with_1);
+        int start_with_1 = n-start_with_0;
+        
+        return min(start_with_1, start_with_0);
+        
     }
-};
+};  
