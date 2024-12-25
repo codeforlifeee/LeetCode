7f925753338fc39f50233c4de122bbd4afdb412e
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2024-12-25 23:13:40 +0530

    Time: 0 ms (100.00%), Space: 13.5 MB (20.61%) - LeetHub

---
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    
+    int t[366];
+    int solve(vector<int>& days, vector<int>& costs, int index) {
+        int n = days.size();
+        
+        if(index >= n) return 0;
+        
+        if(t[index] != -1) return t[index];
+
+        int cost1 = costs[0] + solve(days, costs, index + 1);
+        
+
+        int i = index;
+        
+        
+        while (i < n && days[i] < days[index] + 7) {
+            i++;
+        }
+        
+        
+        int cost2 = costs[1] + solve(days, costs, i);
+
+        i = index;
+        
+        
+        while (i < n && days[i] < days[index] + 30) {
+            i++;
+        }
+        
+        
+        int cost3 = costs[2] + solve(days, costs, i);
+        
+
+        return t[index] = min({cost1, cost2, cost3});
+    }
+
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        
+        memset(t,-1,sizeof(t));
+        
+        return solve(days, costs, 0); 
+    }
+};
