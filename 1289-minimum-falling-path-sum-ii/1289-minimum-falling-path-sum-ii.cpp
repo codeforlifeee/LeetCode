941356e8f6cfa02b1e75df945a2ed97b64169643
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2024-12-25 16:16:18 +0530

    Time: 258 ms (40.95%), Space: 19.9 MB (14.52%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -1,55 +1,26 @@
 class Solution {
 public:
-    int t[300][300];
-    
-    int solve(vector<vector<int>>& matrix, int i, int j) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-
-        if (j < 0 || j >= m) {
-            return INT_MAX;
-        }
-
-        if (i == n - 1) {
-            return matrix[i][j];
-        }
-
-        if (t[i][j] != INT_MAX) {
-            return t[i][j];
+    int minFallingPathSum(vector<vector<int>>& A) {
+        int m = A.size();
+        vector<vector<int>> t(m, vector<int>(m, INT_MAX));
+        
+        // Initialization
+        for (int col = 0; col < m; col++) {
+            t[0][col] = A[0][col];
         }
         
-        int minPath = INT_MAX;
-        for (int k = 0; k < m; k++) {
-            if (k != j) { 
-                minPath = min(minPath, solve(matrix, i + 1, k));
+        for (int row = 1; row < m; row++) {
+            for (int col = 0; col < m; col++) {
+                for (int prevCol = 0; prevCol < m; prevCol++) {
+                    if (prevCol != col) { // Ensure non-zero shift
+                        
+                        t[row][col] = min(t[row][col], A[row][col] + t[row - 1][prevCol]);
+                        
+                    }
+                }
             }
         }
 
-
-        // int leftDiagonal = solve(matrix, i + 1, j - 1);
-        // int down = solve(matrix, i + 1, j);
-        // int rightDiagonal = solve(matrix, i + 1, j + 1);
-        
-        return t[i][j] = matrix[i][j] + minPath;
-    }
-
-    int minFallingPathSum(vector<vector<int>>& matrix) {
-        
-        int n = matrix.size();
-        int m = matrix[0].size();
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                t[i][j] = INT_MAX;
-            }
-        }
-        
-        int result = INT_MAX;
-
-        for (int j = 0; j < m; j++) {
-            result = min(result, solve(matrix, 0, j));
-        }
-
-        return result;
+        return *min_element(t[m - 1].begin(), t[m - 1].end());
     }
 };
