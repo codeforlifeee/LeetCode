36b1ee62f1c5e532b066bafd9b0b5b9059b77769
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2024-12-25 14:12:13 +0530

    Time: 2 ms (6.03%), Space: 8.6 MB (8.03%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    int t[46];
-    int solve(int n){
-        
-        if(t[n] != -1){
-            return t[n];
-        }
-         
-        if(n==1) return 1;
-        if(n==2) return 2;
-        //if(n==3) return 3;
-        
-        return t[n] = solve(n-1) + solve(n-2);
-    }
-   
     int climbStairs(int n) {
+        if(n == 1 || n == 2)
+            return n;
         
-        memset(t,-1, sizeof(t));     
-        return solve(n);
+        vector<int> t(n+1);
+        
+        //t[i] = total ways to climb i stairs
+        t[0] = 0;
+        t[1] = 1;
+        t[2] = 2;
+        
+        for(int i = 3; i<=n; i++) {
+            t[i] = t[i-1] + t[i-2];
+        }
+        
+        return t[n];
     }
 };
