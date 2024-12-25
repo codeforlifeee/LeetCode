d782fd2148452021d737e76c11b7940f1d1bcd92
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2024-12-25 16:13:34 +0530

    Time: 3 ms (31.76%), Space: 15.1 MB (19.88%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,48 +1,26 @@
 class Solution {
 public:
-    int t[300][300];
-    
-    int solve(vector<vector<int>>& matrix, int i, int j) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-
-        if (j < 0 || j >= m) {
-            return INT_MAX;
-        }
-
-        if (i == n - 1) {
-            return matrix[i][j];
-        }
-
-        if (t[i][j] != INT_MAX) {
-            return t[i][j];
-        }
-
-        int leftDiagonal = solve(matrix, i + 1, j - 1);
-        int down = solve(matrix, i + 1, j);
-        int rightDiagonal = solve(matrix, i + 1, j + 1);
-
-        // Store and return the result
-        return t[i][j] = matrix[i][j] + min({leftDiagonal, down, rightDiagonal});
-    }
-
-    int minFallingPathSum(vector<vector<int>>& matrix) {
+    int MFS(vector<vector<int>>& A) {
+        int m = A.size();
+        vector<vector<int>> t(m, vector<int>(m));
         
-        int n = matrix.size();
-        int m = matrix[0].size();
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                t[i][j] = INT_MAX;
+        //initialization
+        for(int col = 0; col<m; col++) {
+            t[0][col] = A[0][col];
+        }
+        
+        for(int row = 1; row < m; row++) {
+            for(int col = 0; col < m; col++) {
+                t[row][col] = A[row][col] + min({t[row-1][col], 
+                                                 t[row-1][max(0, col-1)], 
+                                                 t[row-1][min(m-1, col+1)]});
             }
         }
+
+        return *min_element(t[m-1].begin(), t[m-1].end());
         
-        int result = INT_MAX;
-
-        for (int j = 0; j < m; j++) {
-            result = min(result, solve(matrix, 0, j));
-        }
-
-        return result;
+    }
+    int minFallingPathSum(vector<vector<int>>& A) { 
+        return MFS(A);
     }
 };
