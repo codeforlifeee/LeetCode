50bb96c715375c02781b461db01a085e04737fc8
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2024-12-26 11:48:17 +0530

    Time: 383 ms (33.24%), Space: 90 MB (6.35%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,32 +1,41 @@
 class Solution {
 public:
-    int t[2001][2001];
+    //int t[2001][2001];
     
-    int solve(vector<int>&nums, int i , int currsum, int target){
+    int solve(vector<int>&nums, int i , int currsum, int target, unordered_map<string, int>&mp){
         
         if(i == nums.size()){
             if(currsum == target) return 1;
             else return 0;
         }
         
-        if(t[i][currsum + 1000] != -1){
-            return t[i][currsum + 1000];
+//         if(t[i][currsum + 1000] != -1){
+//             return t[i][currsum + 1000];
+//         }
+        
+        string key = to_string(i) + "_" + to_string(currsum);
+        
+        if(mp.count(key)){
+            return mp[key];
         }
         
-        int take = solve(nums, i+1, currsum + nums[i], target);
+        int take = solve(nums, i+1, currsum + nums[i], target, mp);
         
-        int nottake = solve(nums, i+1, currsum-nums[i] , target);
+        int nottake = solve(nums, i+1, currsum-nums[i] , target, mp);
         
         
-        return t[i][currsum + 1000] = take + nottake;
+        return mp[key] =  take + nottake;
     }
     
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
         
-        memset(t,-1,sizeof(t));
+        unordered_map<string, int>mp;
         
-        return solve(nums, 0,0,target);
+        
+       // memset(t,-1,sizeof(t));
+        
+        return solve(nums, 0,0,target, mp);
         
         
     }
