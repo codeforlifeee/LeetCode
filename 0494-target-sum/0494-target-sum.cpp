f8bdb5d788f10cdecf941127f930e889048146d0
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2024-12-26 11:30:24 +0530

    Time: 62 ms (48.54%), Space: 29.1 MB (24.45%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    int t[2001][2001];
+    
     int solve(vector<int>&nums, int i , int currsum, int target){
         
         if(i == nums.size()){
@@ -7,16 +9,22 @@ public:
             else return 0;
         }
         
+        if(t[i][currsum + 1000] != -1){
+            return t[i][currsum + 1000];
+        }
+        
         int take = solve(nums, i+1, currsum + nums[i], target);
         
         int nottake = solve(nums, i+1, currsum-nums[i] , target);
         
         
-        return take + nottake;
+        return t[i][currsum + 1000] = take + nottake;
     }
     
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
+        
+        memset(t,-1,sizeof(t));
         
         return solve(nums, 0,0,target);
         
