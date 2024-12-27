d3b15f4ef9c79d8938d086020153c149124fe41c
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2024-12-27 13:23:05 +0530

    Time: 7 ms (57.53%), Space: 16.2 MB (44.01%) - LeetHub

---
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    const int M = 1e9+7;
+    int count = 0;
+    int t[100001];
+    int solve(int low, int high,int zero, int one,int l){
+        
+        if(l > high) return 0;
+        
+        if(t[l] != -1) return t[l];
+        
+        int count = (l >= low) ? 1 : 0;
+        
+        int add0 = solve(low, high, zero, one, l + zero);
+        int add1 = solve(low, high, zero, one, l + one);
+
+        return t[l] = (count + add0 + add1)%M;
+        
+    }
+    
+    int countGoodStrings(int low, int high, int zero, int one) {
+        
+        memset(t,-1,sizeof(t));
+        
+        return solve(low,high,zero,one,0);
+        
+    }
+};
