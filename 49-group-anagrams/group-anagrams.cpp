2f4f5ade377a5715f696a843f5ca9c50acbe71a1
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2025-05-22 09:54:56 +0530

    Time: 16 ms (63.31%) | Memory: 23.9 MB (93.84%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<string, vector<string>> anagramMap;
+    const int MOD = 1e9 + 7;
 
-        for (string word : strs) {
-            string sortedWord = word;
-            sort(sortedWord.begin(), sortedWord.end());
-            anagramMap[sortedWord].push_back(word);
+    long long get_hash(string s) {
+        sort(s.begin(), s.end()); 
+        long long h = 0;
+        for (char c : s) {
+            h = (h * 31 + (c - 'a' + 1)) % MOD;
+        }
+        return h;
+    }
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<long long, vector<string>> hashMap;
+
+        for (const string& word : strs) {
+            long long h = get_hash(word);
+            hashMap[h].push_back(word);
         }
 
         vector<vector<string>> result;
-        for (auto& pair : anagramMap) {
+        for (auto& pair : hashMap) {
             result.push_back(pair.second);
         }
 
         return result;
-
+ 
     }
 };
