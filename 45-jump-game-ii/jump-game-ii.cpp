53d7a174404af7794c9a60d07454c98d6bf112de
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2025-05-24 19:23:10 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (62.65%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -4,11 +4,9 @@ public:
             int n = nums.size();
     int jumps = 0, currentEnd = 0, farthest = 0;
 
-    // We do not need to check the last element
     for (int i = 0; i < n - 1; ++i) {
         farthest = max(farthest, i + nums[i]);
 
-        // When we reach the end of the current jump
         if (i == currentEnd) {
             jumps++;
             currentEnd = farthest;
