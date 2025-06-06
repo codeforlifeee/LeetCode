1aabb568394c515a5b26bb718036d8c1224ce9ef
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2025-06-06 17:34:02 +0530

    Time: 3 ms (81.36%) | Memory: 53.1 MB (53.62%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        unordered_set<int> freq(nums.begin(), nums.end());
-        vector<int> result;
+        for (int num : nums) {
+            int idx = abs(num) - 1;
+            if (nums[idx] > 0) {
+                nums[idx] = -nums[idx];  // Mark as seen by negating
+            }
+        }
 
-        for (int i = 1; i <= nums.size(); i++) {
-            if (freq.find(i) == freq.end()) {
-                result.push_back(i);
+        vector<int> result;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0) {
+                result.push_back(i + 1);  // i+1 is missing
             }
         }
 
