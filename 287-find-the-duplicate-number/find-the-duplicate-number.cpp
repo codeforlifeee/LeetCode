d98654ad93ef3c9ea8427a1561063996065ad8b8
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2025-06-06 16:44:47 +0530

    Time: 36 ms (25.54%) | Memory: 64.9 MB (99.26%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        unordered_map<int,int>freq;
-        for(int it : nums){
-            freq[it]++;
-        }
-        int ans = 0;
-        for(auto it : freq){
-            if(it.second >= 2){
-                ans = it.first;
+                sort(nums.begin(), nums.end());
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1]) {
+                return nums[i];
             }
         }
-
-    return ans;
+        return -1;
 
         
     }
