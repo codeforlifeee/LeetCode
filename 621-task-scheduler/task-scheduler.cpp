cf3b166155d0d4d2a08101361cd298abb87cabcc
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2025-06-10 22:12:46 +0530

    Time: 0 ms (100.00%) | Memory: 38.2 MB (83.38%) - LeetSync

---
+++ 621-task-scheduler/task-scheduler.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+          vector<int> freq(26, 0);
+    for (char task : tasks) {
+        freq[task - 'A']++;
+    }
+
+    int maxFreq = *max_element(freq.begin(), freq.end());
+    int maxCount = count(freq.begin(), freq.end(), maxFreq);
+
+    return max((int)tasks.size(), (maxFreq - 1) * (n + 1) + maxCount);
+
+        
+    }
+};
