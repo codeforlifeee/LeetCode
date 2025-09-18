5d7893afe1655b48f122255411bbf22554e0dcbb
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2025-09-18 21:07:08 +0530

    Time: 4 ms (48.35%) | Memory: 18.5 MB (66.22%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        auto cmp = [](ListNode* a, ListNode* b) { return a->val > b->val; };
+        auto cmp = [](ListNode* a, ListNode* b){ return a->val > b->val; };
         priority_queue<ListNode*, vector<ListNode*>, decltype(cmp)> pq(cmp);
-
         for (auto l : lists) if (l) pq.push(l);
-
-        ListNode dummy(0);
-        ListNode* tail = &dummy;
-
+        ListNode dummy(0), *tail=&dummy;
         while (!pq.empty()) {
             ListNode* node = pq.top(); pq.pop();
-            tail->next = node;
-            tail = node;
+            tail->next=node; tail=node;
             if (node->next) pq.push(node->next);
         }
         return dummy.next;
