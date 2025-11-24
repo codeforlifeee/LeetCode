61627a3def932d6e5c6b5009948c577a618d551e
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2025-11-24 23:28:40 +0530

    Time: 50 ms (12.62%) | Memory: 46.5 MB (7.28%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
@@ -4,23 +4,23 @@ public:
     vector<int> nums;
     int n;
 
-    int dfs(int i, int r) {
+    // solve(index, remainder)
+    int solve(int i, int r) {
         if (i == n) {
-            // If remainder is zero, valid sum = 0
+            // If we ended with remainder 0 → valid
             return (r == 0 ? 0 : INT_MIN/2);
         }
 
-        if (memo[i][r] != INT_MIN) 
+        if (memo[i][r] != INT_MIN)
             return memo[i][r];
 
-        // Option 1: Skip current element
-        int skip = dfs(i + 1, r);
+        // Option 1: skip current element
+        int skip = solve(i + 1, r);
 
-        // Option 2: Take current element
-        int take = nums[i] + dfs(i + 1, (r + nums[i]) % 3);
+        // Option 2: take current element
+        int take = nums[i] + solve(i + 1, (r + nums[i]) % 3);
 
-        // Best of both
-        return memo[i][r] = max(take, skip);
+        return memo[i][r] = max(skip, take);
     }
 
     int maxSumDivThree(vector<int>& arr) {
@@ -29,7 +29,9 @@ public:
 
         memo.assign(n, vector<int>(3, INT_MIN));
 
-        int ans = dfs(0, 0);
+        int ans = solve(0, 0);
+
+        // If impossible (shouldn't happen), return 0
         return ans < 0 ? 0 : ans;
     }
 };
