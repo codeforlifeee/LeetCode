13da264546ec730361d3f34b219d8a2426001533
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2025-12-30 14:35:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (26.11%) - LeetSync

---
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAllBitsSet(int x) {
+        return (x & (x+1)) == 0;
+    }
+    int smallestNumber(int n) {
+        int result = n;
+
+        while(!isAllBitsSet(result)) {
+            result++;
+        }
+
+        return result;
+    }
+};
+
+
