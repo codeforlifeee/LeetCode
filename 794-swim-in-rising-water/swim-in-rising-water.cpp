7219e62afe91d12bc1c2d14d5efd318d6c680e15
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2026-01-01 10:08:47 +0530

    Time: 35 ms (10.10%) | Memory: 17.7 MB (9.86%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -1,44 +1,47 @@
 class Solution {
 public:
-    vector<vector<int>> directions{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    using P = pair<int, pair<int, int>>; // {time, {i, j}}
-
     int swimInWater(vector<vector<int>>& grid) {
         int n = grid.size();
-        vector<vector<int>> result(n, vector<int>(n, INT_MAX));
-        
-        priority_queue<P, vector<P>, greater<P>> pq; // min-heap by time
-        result[0][0] = grid[0][0];
-        pq.push({grid[0][0], {0, 0}});
+
+        // Min-heap: {time, row, col}
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+
+        vector<vector<bool>> visited(n, vector<bool>(n, false));
+
+        // Start from (0,0)
+        pq.push({grid[0][0], 0, 0});
+
+        vector<vector<int>> directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
 
         while (!pq.empty()) {
-            int currTime = pq.top().first;
-            auto cell = pq.top().second;
-            int i = cell.first;
-            int j = cell.second;
+            auto curr = pq.top();
             pq.pop();
 
-            // if we reached destination, return time
+            int time = curr[0];
+            int i = curr[1];
+            int j = curr[2];
+
+            // Destination reached
             if (i == n - 1 && j == n - 1)
-                return currTime;
+                return time;
 
-            // Skip if we’ve already found a better path
-            if (currTime > result[i][j]) continue;
+            if (visited[i][j])
+                continue;
 
-            for (auto& dir : directions) {
-                int i_ = i + dir[0];
-                int j_ = j + dir[1];
-                if (i_ >= 0 && i_ < n && j_ >= 0 && j_ < n) {
-                    int nextTime = max(currTime, grid[i_][j_]);
+            visited[i][j] = true;
 
-                    if (nextTime < result[i_][j_]) {
-                        result[i_][j_] = nextTime;
-                        pq.push({nextTime, {i_, j_}});
-                    }
-                }
+            for (auto &dir : directions) {
+                int ni = i + dir[0];
+                int nj = j + dir[1];
+
+                if (ni < 0 || nj < 0 || ni >= n || nj >= n || visited[ni][nj])
+                    continue;
+
+                int newTime = max(time, grid[ni][nj]);
+                pq.push({newTime, ni, nj});
             }
         }
 
-        return -1; // should never reach here
+        return -1; // unreachable (won't happen due to problem constraints)
     }
 };
