4f90d268320f347945f0e6b0a75b54260f89708c
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2026-01-18 16:04:10 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (89.54%) - LeetSync

---
+++ 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int t[50][50];
+
+    int solve(vector<int>& values, int i, int j) {
+        if (j - i < 2) 
+            return 0; // less than 3 vertices -> no triangle
+
+        if (t[i][j] != -1)
+            return t[i][j];
+
+        int result = INT_MAX;
+        for (int k = i + 1; k < j; ++k) {
+            int weight = solve(values, i, k) +
+                        values[i] * values[k] * values[j] +
+                        solve(values, k, j);
+            result = min(result, weight);
+        }
+
+        return t[i][j] = result;
+    }
+
+    int minScoreTriangulation(vector<int>& values) {
+        memset(t, -1, sizeof(t));
+        return solve(values, 0, values.size() - 1);
+    }
+};
