03ae7bedaefb6619642920e208322561aad41d3b
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2026-01-18 14:37:47 +0530

    Time: 63 ms (52.66%) | Memory: 22.4 MB (69.45%) - LeetSync

---
+++ 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.cpp
@@ -0,0 +1,20 @@
+
+//Approach-2 (O(1) space using two pointers)
+//T.C : O(n^2)
+//S.C : O(1)
+class Solution {
+public:
+    int triangularSum(vector<int>& nums) {
+        int n = nums.size();
+        for (int size = n - 1; size >= 1; size--) {
+
+            for (int i = 0; i < size; i++) {
+                nums[i] = (nums[i] + nums[i + 1]) % 10;
+            }
+            
+        }
+        return nums[0];
+    }
+};
+
+
