ff38ba5ce51bfccea21671e405f446e9aa61eaf2
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2026-01-25 11:09:41 +0530

    Time: 20 ms (79.67%) | Memory: 312.5 MB (45.88%) - LeetSync

--- 4171-rotate-non-negative-elements/rotate-non-negative-elements.cpp
+++ 4171-rotate-non-negative-elements/rotate-non-negative-elements.cpp
@@ -10,11 +10,14 @@ public:
         int m = nonNeg.size();
         if (m <= 1) return nums;
         k = k % m;
+
         vector<int> rotated(m);
         for (int i = 0; i < m; i++) {
             rotated[i] = nonNeg[(i + k) % m];
         }
+
         int idx = 0;
+        
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] >= 0) {
                 nums[i] = rotated[idx++];
