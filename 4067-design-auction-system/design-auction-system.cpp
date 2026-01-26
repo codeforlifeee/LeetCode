3eb44b4516eff33d2ac56780339aef5cf2388fe7
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2026-01-26 23:51:24 +0530

    Time: 193 ms (57.18%) | Memory: 349.4 MB (86.34%) - LeetSync

---
+++ 4067-design-auction-system/design-auction-system.cpp
@@ -0,0 +1,61 @@
+class AuctionSystem {
+private:
+    // itemId -> set of (bidAmount, userId) sorted
+    unordered_map<int, set<pair<int, int>>> itemBids;
+    // (userId, itemId) -> (bidAmount, userId) for quick lookup
+    map<pair<int, int>, pair<int, int>> userBids;
+    
+public:
+    AuctionSystem() {
+        
+    }
+    
+    void addBid(int userId, int itemId, int bidAmount) {
+        auto key = make_pair(userId, itemId);
+        
+        // If user already has a bid on this item, remove it first
+        if (userBids.find(key) != userBids.end()) {
+            itemBids[itemId].erase(userBids[key]);
+        }
+        
+        // Add new bid
+        auto bidPair = make_pair(bidAmount, userId);
+        itemBids[itemId].insert(bidPair);
+        userBids[key] = bidPair;
+    }
+    
+    void updateBid(int userId, int itemId, int newAmount) {
+        auto key = make_pair(userId, itemId);
+        
+        // Remove old bid
+        itemBids[itemId].erase(userBids[key]);
+        
+        // Add updated bid
+        auto bidPair = make_pair(newAmount, userId);
+        itemBids[itemId].insert(bidPair);
+        userBids[key] = bidPair;
+    }
+    
+    void removeBid(int userId, int itemId) {
+        auto key = make_pair(userId, itemId);
+        
+        // Remove from itemBids
+        itemBids[itemId].erase(userBids[key]);
+        if (itemBids[itemId].empty()) {
+            itemBids.erase(itemId);
+        }
+        
+        // Remove from userBids
+        userBids.erase(key);
+    }
+    
+    int getHighestBidder(int itemId) {
+        if (itemBids.find(itemId) == itemBids.end() || itemBids[itemId].empty()) {
+            return -1;
+        }
+        
+        // Get the last element (highest bid, highest userId)
+        auto it = itemBids[itemId].rbegin();
+        return it->second; // return userId
+    }
+};
