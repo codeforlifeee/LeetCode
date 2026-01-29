45ba228b9376347952aff339b8bacdf78bfb9ba5
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2026-01-29 19:07:39 +0530

    Time: 684 ms (6.47%) | Memory: 179.8 MB (9.24%) - LeetSync

--- 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
@@ -1,29 +1,48 @@
+//S.C : O(V+E) -> We create a graph, where V = number of vertices and E = number of edges
 class Solution {
 public:
+    void dijkstra(char &source, unordered_map<char, vector<pair<char, int>>> &adj, vector<vector<long long>> &costMatrix) {
 
-    void FloydWarshall(vector<vector<long long>> &adjMatrix, vector<char>& original, vector<char>& changed, vector<int>& cost) {
+        //min-heap
+        priority_queue<pair<int, char>, vector<pair<int, char>>, greater<pair<int, char>>> pq;
 
-            for(int i = 0; i < original.size(); i++) {
-                int s = original[i] - 'a';
-                int t = changed[i] - 'a';
+        pq.push({0, source});
 
-                adjMatrix[s][t] = min(adjMatrix[s][t], (long long)cost[i]);
-            }
+        while(!pq.empty()) {
+            int d        = pq.top().first;
+            char adjNode = pq.top().second;
+            pq.pop();
 
-            //Apply Floyd Warshall
-            for (int k = 0; k < 26; ++k) {
-                for (int i = 0; i < 26; ++i) {
-                    for (int j = 0; j < 26; ++j) {
-                        adjMatrix[i][j] = min(adjMatrix[i][j], adjMatrix[i][k] + adjMatrix[k][j]); 
-                    }
+            for(auto &it : adj[adjNode]) {
+                char adjNode = it.first;
+                int cost      = it.second;
+
+                if(costMatrix[source - 'a'][adjNode - 'a'] > d + cost) {
+                    costMatrix[source - 'a'][adjNode - 'a'] = d + cost;
+                    pq.push({d + cost, adjNode});
                 }
             }
+
+        }
+        return;
     }
-
+    //V = # of vertices
+    //E = # of edges
+    //O(n * (V+E)log(V))
     long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
-        vector<vector<long long>> adjMatrix(26, vector<long long>(26, INT_MAX));
+        unordered_map<char, vector<pair<char, int>>> adj;
 
-        FloydWarshall(adjMatrix, original, changed, cost); //update adjMatrix with shortest path using Floyd Warshall
+        for(int i = 0; i < original.size(); i++) {
+            adj[original[i]].push_back({changed[i], cost[i]});
+        }
+
+        vector<vector<long long>> costMatrix(26, vector<long long>(26, INT_MAX));
+
+        //Populate the costMatrix using Dijkstra's Algorithm
+        for(int i = 0; i < source.length(); i++) { //n
+            char ch = source[i];
+            dijkstra(ch, adj, costMatrix);
+        }
 
         long long ans = 0;
 
@@ -32,14 +51,15 @@ public:
                 continue;
             }
 
-            if(adjMatrix[source[i] -'a'][target[i] - 'a'] == INT_MAX) {
+            if(costMatrix[source[i] - 'a'][target[i] - 'a'] == INT_MAX) {
                 return -1;
             }
 
-            ans += adjMatrix[source[i] -'a'][target[i] - 'a'];
+            ans += costMatrix[source[i] - 'a'][target[i] - 'a'];
+
         }
 
         return ans;
-
     }
 };
+
