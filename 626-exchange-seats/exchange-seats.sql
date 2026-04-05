be323ca76e187070742b72b2f97881d59ea3a576
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2026-04-05 19:53:21 +0530

    Time: 327 ms (69.50%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,9 +1,9 @@
 SELECT 
+    id,
     CASE 
-        WHEN id % 2 = 1 AND id != (SELECT MAX(id) FROM Seat) THEN id + 1
-        WHEN id % 2 = 0 THEN id - 1
-        ELSE id
-    END AS id,
-    student
-FROM Seat
-ORDER BY id;
+        WHEN id % 2 = 1 
+            THEN COALESCE(LEAD(student) OVER (ORDER BY id), student)
+        ELSE 
+            LAG(student) OVER (ORDER BY id)
+    END AS student
+FROM Seat;
