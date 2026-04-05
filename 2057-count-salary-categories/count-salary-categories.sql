36772175b90264d2b70bd56bd00bbb3ac6953377
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2026-04-05 15:50:27 +0530

    Time: 1639 ms (27.01%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -0,0 +1,15 @@
+SELECT c.category, COALESCE(COUNT(a.income), 0) AS accounts_count
+FROM (
+    SELECT 'Low Salary' AS category
+    UNION ALL
+    SELECT 'Average Salary'
+    UNION ALL
+    SELECT 'High Salary'
+) c
+LEFT JOIN Accounts a
+ON (
+    (c.category = 'Low Salary' AND a.income < 20000) OR
+    (c.category = 'Average Salary' AND a.income BETWEEN 20000 AND 50000) OR
+    (c.category = 'High Salary' AND a.income > 50000)
+)
+GROUP BY c.category;
