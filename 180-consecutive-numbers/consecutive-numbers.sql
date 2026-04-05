0e5df5c88cb74e87d2424fa02aedcc9a7ccd3afa
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2026-04-05 13:39:10 +0530

    Time: 529 ms (61.38%) | Memory: 0B (100.00%) - LeetSync

--- 180-consecutive-numbers/consecutive-numbers.sql
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -1,9 +1,12 @@
-SELECT DISTINCT num AS ConsecutiveNums
-FROM (
-    SELECT 
-        num,
-        LEAD(num, 1) OVER (ORDER BY id) AS next1,
-        LEAD(num, 2) OVER (ORDER BY id) AS next2
-    FROM Logs
-) t
-WHERE num = next1 AND num = next2;
+SELECT DISTINCT
+    l1.Num AS ConsecutiveNums
+FROM
+    Logs l1,
+    Logs l2,
+    Logs l3
+WHERE
+    l1.Id = l2.Id - 1
+    AND l2.Id = l3.Id - 1
+    AND l1.Num = l2.Num
+    AND l2.Num = l3.Num
+;
