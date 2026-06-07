75914b0a750eb80188334101c4370396c479eb5c
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2026-06-07 14:03:22 +0530

    Time: 686 ms (5.00%) | Memory: 247.6 MB (5.20%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        vector<int> store(256, 0);
-        int l = 0, r = 0, ans = 0;
-        while(r < s.length()){
-            store[s[r]]++;
-            while(store[s[r]]>1){
-                store[s[l]]--;
-                l++;
-            }
-            ans = max(ans, r-l+1);
-            r++;
+    int lengthOfLongestSubstring(string str) {
+            int maxans = 0;
+
+    for(int i = 0; i < str.length(); i++) {
+        unordered_set<char> st;
+
+        for(int j = i; j < str.length(); j++) {
+            if(st.find(str[j]) != st.end())
+                break;
+
+            st.insert(str[j]);
+            maxans = max(maxans, j - i + 1);
         }
-        return ans;
-    
+    }
+
+    return maxans;
+
+        
     }
 };
