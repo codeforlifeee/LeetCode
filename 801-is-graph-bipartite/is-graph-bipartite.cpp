b1f882b6cbf4b76fa7b089af96d25b730b09ae48
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2026-07-25 20:57:29 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (14.16%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -3,13 +3,16 @@ public:
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
         vector<int> color(n, -1);
+        // -1 implies not coloured
 
         for (int i = 0; i < n; i++) {
             if (color[i] != -1) continue;
+            // If node i has already been colored (visited),
+            // skip it and move to the next node.
 
             queue<int> q;
-            q.push(i);
             color[i] = 0;
+            q.push(i);
 
             while (!q.empty()) {
                 int node = q.front();
