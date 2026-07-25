cb74403cfc2da7870b493ede9b03e8b92ccc0fb0
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2026-07-25 09:52:59 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (42.30%) - LeetSync

--- 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
@@ -1,33 +1,20 @@
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
     int maxProduct(int n) {
-        int max1 = -1, max2 = -1;
-        vector<int> hashTable(10, 0);
+        vector<int> digits;
 
         while (n != 0) {
-            int digit = n % 10;
-            hashTable[digit]++;
+            digits.push_back(n % 10);
             n /= 10;
         }
 
-        for (int i = 9; i >= 0; i--) {
-            if (hashTable[i] >= 2 && max1 == -1 && max2 == -1) {
-                max1 = i;
-                max2 = i;
-                break;
-            }
+        sort(digits.begin(), digits.end());
 
-            if (hashTable[i] > 0) {
-                if (max1 == -1)
-                    max1 = i;
-                else if (max2 == -1 && max1 != i)
-                    max2 = i;
-            }
-        }
-
-        return max1 * max2;
+        int m = digits.size();
+        return digits[m - 1] * digits[m - 2];
     }
 };
