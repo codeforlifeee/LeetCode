8d332a80bbe97d1061be68fb7cdcba24f79de70d
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2026-08-17 00:04:16 +0530

    Time: 7 ms (39.59%) | Memory: 131.2 MB (92.89%) - LeetSync

--- 2156-stone-game-ix/stone-game-ix.cpp
+++ 2156-stone-game-ix/stone-game-ix.cpp
@@ -1,19 +1,20 @@
+
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
 class Solution {
 public:
-    bool stoneGameIX(vector<int>& stones) {
-        int cnt0 = 0, cnt1 = 0, cnt2 = 0;
-        for (int val : stones) {
-            if (int type = val % 3; type == 0) {
-                ++cnt0;
-            } else if (type == 1) {
-                ++cnt1;
-            } else {
-                ++cnt2;
-            }
+    bool stoneGameIX(std::vector<int>& stones) {
+        int count[3] = {0, 0, 0};
+        for (int stone : stones) {
+            count[stone % 3]++;
         }
-        if (cnt0 % 2 == 0) {
-            return cnt1 >= 1 && cnt2 >= 1;
+        
+        if (count[0] % 2 == 0) {
+            return count[1] > 0 && count[2] > 0;
         }
-        return cnt1 - cnt2 > 2 || cnt2 - cnt1 > 2;
+        
+        return std::abs(count[1] - count[2]) > 2;
     }
 };
