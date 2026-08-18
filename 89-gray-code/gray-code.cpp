d1f126b8ea5926c632db2967fae6d291b241bfae
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2026-08-18 23:21:26 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (90.01%) - LeetSync

---
+++ 89-gray-code/gray-code.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        int size = 1 << n;
+        vector<int> result;
+
+        result.reserve(size);
+
+        for(int i =0; i<size; i++){
+            result.push_back(i ^ ( i>> 1));
+        }
+        return result;
+
+    }
+};
