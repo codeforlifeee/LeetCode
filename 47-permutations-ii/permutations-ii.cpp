78342fda727f9805a6b21d89f36c7f1c0fd15a1e
Tejas Surya <168173457+codeforlifeee@users.noreply.github.com>
2026-08-22 17:51:16 +0530

    Time: 43 ms (7.77%) | Memory: 12.5 MB (38.20%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.cpp
@@ -0,0 +1,98 @@
+class Solution {
+public:
+    vector<int>p;
+    vector<bool>vis;
+    set<vector<int>>res;
+
+
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+
+        p.clear();
+        res.clear();
+        vis.assign(nums.size(), false);
+
+        
+         solve(nums,p,vis, res);
+        vector<vector<int>> finalResult;
+
+
+        for(auto it : res){
+            finalResult.push_back(it);
+        }
+        return finalResult;
+    }
+
+    
+
+void solve(vector<int>&nums, vector<int>& p, vector<bool>& vis, set<vector<int>>& res){
+    if(p.size() == nums.size()) { res.insert(p); return;}
+
+    for(int i = 0; i< nums.size(); i++){
+        if(!vis[i]){
+            vis[i] = true;
+
+            p.push_back(nums[i]);
+
+
+            solve(nums, p, vis, res);
+
+            // backtracking it
+
+            vis[i] = false;
+
+            p.pop_back();
+        }
+    }
+}
+
+};
+
+
+    // base case 
+    // if the p.size() == nums.size() 
+    // insert the p in the set and return from there
+    // 
+
+    //  p => [2,1,1] // nums[ 1,1,2] 
+
+/// sizes are the same
+// 
+
+
+// Backtracking approach
+// My approach:
+// Generate all the Permutations
+// use set to filter the duplicates
+// like suppose
+// [1,1,2]:
+//1.  path = [1] , rem  = [1,2] -> recurse
+//2. path =[1,1] rem = [2] -> recurse
+// 3. path = [1,1,2] , rem = [] --->> so add it to the set as vector
+// 4. Backtracking -> path = [1,2], rem [1] 0> path [1,2,1,] -> add this to the set
+// 5.  backtracking -> path = [2] , rem  = [1,1] -> path [ 2, 1,1] -> add this to the set
+
+//SHORTCOMING:
+// TC : O(N*N!) 
+// SPACE : O(N*N!) 
+
+// OPTIMIMSED
+// TC: O(N*N!)
+// SC: O(N) ===> RECURSION STACK
+
+
+// nums= [1,1,2]
+
+// // step 1 : sorting it
+// nums = [1,1,2]
+// // after sorting 
+// nums = [1,1,2]
+
+// // step2 :
+// // i = 0 i.e. element = 1 so we will pick it 
+
+// // vis[0] = true;
+// // i  = 1  pick 1  vis[1] = true
+// // vis[1] = true path =[1,1,2]
+
+// // i = 0  , i = 1 skiping nums[1] == nums[0] & !vis[0]
+
